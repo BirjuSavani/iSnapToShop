f181ba9fa7e5d044c19a9d6717ec9653bfe67fe4
BirjuSavani <savanibirju710@gmail.com>
2025-12-16 10:08:26 +0530

    change version for gofynd

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ EXPOSE 8080
 WORKDIR /app
 COPY ./package.json .
 COPY ./package-lock.json .
-RUN npm install
+RUN npm install --legacy-peer-deps
 COPY . .
-RUN cd frontend && npm install && npm run build
+RUN cd frontend && npm install --legacy-peer-deps && npm run build
 CMD ["npm", "run", "start:prod"]
--- src/fdkConfig/fdkConfig.js
+++ src/fdkConfig/fdkConfig.js
@@ -10,8 +10,8 @@ const sqliteInstance = new sqlite3.Database('session_storage.db');
  * Initialize the FDK extension with configuration and webhook handlers
  */
 const fdkExtension = setupFdk({
-  api_key: "684185bd9b31e867a904ff7a",
-  api_secret: "Eu3XLw0gVN2C.NS",
+  api_key: process.env.EXTENSION_API_KEY,
+  api_secret: process.env.EXTENSION_API_SECRET,
   base_url: process.env.EXTENSION_BASE_URL,
   cluster: process.env.FP_API_DOMAIN,
   callbacks: {
